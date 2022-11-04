864367d8c569289de93e7bf571700740c95344fd
LikithKumarDK <likithcse168@gmail.com>
2022-11-04 17:03:46 +0530

    Update: Created setup of react with MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
 
 function App() {
-	return <h1>Hello world! I am using React</h1>;
+	return <Typography>Hello world! I am using React</Typography>;
 }
 
 export default App;
